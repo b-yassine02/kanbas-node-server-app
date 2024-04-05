9b5d15644861fca32bbe3b72d256fcf83582cbf6
Yassine Bouanani <ysb@Yassines-MBP.mynetworksettings.com>
2024-04-05 14:22:13 -0400

    mongo a6

---
+++ App.js
@@ -0,0 +1,50 @@
+import express from "express";
+import Hello from "./Hello.js";
+import Lab5 from "./Lab5.js";
+import CourseRoutes from "./Kanbas/courses/routes.js";
+import ModuleRoutes from "./Kanbas/modules/routes.js";
+import cors from "cors";
+import mongoose from "mongoose";
+import UserRoutes from "./Users/routes.js";
+import "dotenv/config";
+import session from "express-session";
+
+const CONNECTION_STRING =
+  process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+
+mongoose.connect(CONNECTION_STRING);
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
+
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+};
+
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+
+app.use(session(sessionOptions));
+
+app.use(express.json());
+
+Lab5(app);
+Hello(app);
+CourseRoutes(app);
+ModuleRoutes(app);
+
+UserRoutes(app);
+
+app.listen(process.env.PORT || 4000);
